02781c974cf7183acf98d0440a00e5d042301168
Ian Pennington <Ian.Pennington@open-money.co.uk>
2021-10-05 16:00:00 +0100

    Tests for mortgage client

--- OpenMoney.InterviewExercise.Tests/MortgageQuoteClientFixture.cs
+++ OpenMoney.InterviewExercise.Tests/MortgageQuoteClientFixture.cs
@@ -7,36 +7,63 @@ namespace OpenMoney.InterviewExercise.Tests
 {
     public class MortgageQuoteClientFixture
     {
-        private readonly Mock<IThirdPartyHomeInsuranceApi> _apiMock = new();
+        private readonly Mock<IThirdPartyMortgageApi> _apiMock = new();
 
         [Fact]
-        public void GetQuote_ShouldReturnNull_IfHouseValue_Over10Mill()
+        public void GetQuote_ShouldReturnAQuote()
         {
-            const decimal houseValue = 10_000_001m;
-            
-            var mortgageClient = new HomeInsuranceQuoteClient(_apiMock.Object);
-            var quote = mortgageClient.GetQuote(houseValue);
-            
+            const decimal houseValue = 100_000m;
+            const decimal deposit = 10_000m;
+            const decimal mortgageAmount = houseValue - deposit;
+            const decimal monthlyPayment = 100m;
+
+            _apiMock
+                .Setup(api => api.GetQuotes(It.Is<ThirdPartyMortgageRequest>(r => r.MortgageAmount == mortgageAmount)))
+                .ReturnsAsync(new[]
+                {
+                    new ThirdPartyMortgageResponse { MonthlyPayment = monthlyPayment }
+                });
+
+            var mortgageClient = new MortgageQuoteClient(_apiMock.Object);
+            var quote = mortgageClient.GetQuote(houseValue, deposit);
+
+            Assert.Equal(monthlyPayment, (decimal)quote.MonthlyPayment);
+        }
+
+        [Fact]
+        public void GetQuote_WhenDepositIsLessThanThreshold_ThenShouldReturnNull()
+        {
+            const decimal houseValue = 100_000m;
+            const decimal deposit = 1_000m;
+
+            var mortgageClient = new MortgageQuoteClient(_apiMock.Object);
+            var quote = mortgageClient.GetQuote(houseValue, deposit);
+
             Assert.Null(quote);
         }
 
         [Fact]
-        public void GetQuote_ShouldReturn_AQuote()
+        public void GetQuote_WhenMultipleQuotesReturned_THenShouldReturnTheCheapest()
         {
             const decimal houseValue = 100_000m;
+            const decimal deposit = 10_000m;
+            const decimal mortgageAmount = houseValue - deposit;
+            const decimal monthlyPayment = 100m;
+            const decimal difference = 10m;
 
             _apiMock
-                .Setup(api => api.GetQuotes(It.Is<ThirdPartyHomeInsuranceRequest>(r =>
-                    r.ContentsValue == HomeInsuranceQuoteClient.ContentsValue && r.HouseValue == houseValue)))
+                .Setup(api => api.GetQuotes(It.Is<ThirdPartyMortgageRequest>(r => r.MortgageAmount == mortgageAmount)))
                 .ReturnsAsync(new[]
                 {
-                    new ThirdPartyHomeInsuranceResponse { MonthlyPayment = 30 }
+                    new ThirdPartyMortgageResponse { MonthlyPayment = monthlyPayment - difference },
+                    new ThirdPartyMortgageResponse { MonthlyPayment = monthlyPayment },
+                    new ThirdPartyMortgageResponse { MonthlyPayment = monthlyPayment + difference }
                 });
-            
-            var mortgageClient = new HomeInsuranceQuoteClient(_apiMock.Object);
-            var quote = mortgageClient.GetQuote(houseValue);
-            
-            Assert.Equal(30m, (decimal)quote.MonthlyPayment);
+
+            var mortgageClient = new MortgageQuoteClient(_apiMock.Object);
+            var quote = mortgageClient.GetQuote(houseValue, deposit);
+
+            Assert.Equal(monthlyPayment - difference, (decimal)quote.MonthlyPayment);
         }
     }
 }
